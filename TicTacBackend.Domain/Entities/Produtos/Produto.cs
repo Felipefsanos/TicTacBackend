c0bfedce631f485a152e308c7d8bc1226b99020a
conta.comercial.tictac <conta.comercial.tictac@gmail.com>
2021-03-09 14:09:04 -0300

    Adicionando entidade

---
+++ TicTacBackend.Domain/Entities/Produtos/Produto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TicTacBackend.Domain.Entities.Base;
+
+namespace TicTacBackend.Domain.Entities.Produtos
+{
+    public class Produto : EntidadeBase
+    {
+        public string Descricao { get; set; }
+        public string Nome { get; set; }
+        public decimal Valor { get; set; }
+    }
+}
---
+++ TicTacBackend.Infra.Data/Migrations/20210309153050_CreateProdutos.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TicTacBackend.Infra.Data.Migrations
+{
+    public partial class CreateProdutos : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Produtos",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Descricao = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Valor = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Produtos", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Produtos");
+        }
+    }
+}
